9a1c2b13492b649b3b938e5d3beea4cf25b358e8
Matias Ruiz <39162132+matiasaruiz@users.noreply.github.com>
2022-08-22 14:23:01 -0300

    termie :)

--- web_scrapping.py
+++ web_scrapping.py
@@ -2,16 +2,37 @@ import bs4
 import requests
 
 
-resultado = requests.get('https://www.escueladirecta.com/courses')
+# Goal: obtener todos los tittulos de los libros que tiene 4 o mas estrllas
 
+# cada libro tiene una class=star-rating Three y necesito class=star-rating Four and class=star-rating Five
+ 
 
-navegable = bs4.BeautifulSoup(resultado.text,'lxml')
+ # Url independiente del numero de pagina
 
-imgs = navegable.select('.course-box-image')[0]['src']
+url_root = 'http://books.toscrape.com/catalogue/page-{}.html'
 
-imagen_extraida = requests.get(imgs)
+# Lista en la cual se van a guardar los aciertos de 4 o 5 estrllas
+aciertos = []
 
+# Recorro todas las paginas
+for pagina in range(0,51):
 
-f = open("img_test.jpg", 'wb')
-f.write(imagen_extraida.content)
-f.close()
+    # Creo los navegales por pagina
+    url_pagina = url_root.format(pagina)
+    resultado = requests.get(url_pagina)
+    navegable = bs4.BeautifulSoup(resultado.text,'lxml')
+
+    # Extraigo los datos de los libros
+    libros = navegable.select('.product_pod')
+
+    # Busco aciertos de libros y los guaro en la lista
+    for libro in libros:
+
+        if len(libro.select('.star-rating.Four')) != 0 or len(libro.select('.star-rating.Five')) != 0:
+        
+            titulo_acierto = libro.select('a')[1]['title']
+            aciertos.append(titulo_acierto)
+
+# Muestro los aciertos
+for libro in aciertos:
+    print(libro)
