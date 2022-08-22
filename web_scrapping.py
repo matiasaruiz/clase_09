3cca09c81be0790a350c470ad34a9c7cf6734014
Matias Ruiz <39162132+matiasaruiz@users.noreply.github.com>
2022-08-22 12:22:35 -0300

    test de extracion de imagenes y save en archivo

--- web_scrapping.py
+++ web_scrapping.py
@@ -2,12 +2,16 @@ import bs4
 import requests
 
 
-resultado = requests.get('https://escueladirecta-blog.blogspot.com/2021/10/encapsulamiento-pilares-de-la.html')
+resultado = requests.get('https://www.escueladirecta.com/courses')
 
 
 navegable = bs4.BeautifulSoup(resultado.text,'lxml')
 
-sidebar = navegable.select('.sidebar-container div')
+imgs = navegable.select('.course-box-image')[0]['src']
 
-for div in sidebar:
-    print(div.getText())
+imagen_extraida = requests.get(imgs)
+
+
+f = open("img_test.jpg", 'wb')
+f.write(imagen_extraida.content)
+f.close()
